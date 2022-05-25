98ec0de6d6810d099af98b98755201215a63f7d4
MMF <38781556+mmf-Github@users.noreply.github.com>
2022-05-25 13:50:52 +0200

    Update s3.tf

--- demo-terraform/aws/simple-ec2-with-s3-access/s3.tf
+++ demo-terraform/aws/simple-ec2-with-s3-access/s3.tf
@@ -19,10 +19,10 @@ resource "aws_s3_bucket_acl" "bucket" {
 resource "aws_s3_bucket_public_access_block" "bucket" {
     bucket = aws_s3_bucket.bucket.id
 
-    block_public_acls   = true
-    block_public_policy = true
-    ignore_public_acls = true
-    restrict_public_buckets = true
+    block_public_acls   = false
+    block_public_policy = false
+    ignore_public_acls = false
+    restrict_public_buckets = false
 }
 
 # resource "aws_s3_bucket_server_side_encryption_configuration" "bucket" {
